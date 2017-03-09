23c132e6b168d54c76096b600fd2d6c1ed9fa4e5
Lars Briem <lars.briem@kit.edu>
2017-03-09 15:28:21 +0100

    Remove unused method.

--- src/main/java/edu/kit/ifv/mobitopp/publictransport/connectionscan/BasicTimes.java
+++ src/main/java/edu/kit/ifv/mobitopp/publictransport/connectionscan/BasicTimes.java
@@ -49,9 +49,4 @@ abstract class BasicTimes implements Times {
 		return times[internal];
 	}
 
-	protected boolean isTooLateAt(Time departure, Stop end) {
-		Time arrival = getConsideringMinimumChangeTime(end);
-		return arrival.isBefore(departure);
-	}
-
 }
