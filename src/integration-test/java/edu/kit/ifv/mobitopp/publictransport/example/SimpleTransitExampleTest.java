188d489b31fccc5bbadc1c4ce1396b5c8e73e021
Lars Briem <lars.briem@kit.edu>
2017-03-15 10:03:10 +0100

    Add example from many to many stops.

--- src/integration-test/java/edu/kit/ifv/mobitopp/publictransport/example/SimpleTransitExampleTest.java
+++ src/integration-test/java/edu/kit/ifv/mobitopp/publictransport/example/SimpleTransitExampleTest.java
@@ -4,6 +4,9 @@ import static com.github.npathai.hamcrestopt.OptionalMatchers.isPresent;
 import static edu.kit.ifv.mobitopp.publictransport.example.SimpleNetwork.fourOClock;
 import static edu.kit.ifv.mobitopp.publictransport.example.SimpleNetwork.oneOClock;
 import static edu.kit.ifv.mobitopp.publictransport.example.SimpleNetwork.threeOClock;
+import static java.time.temporal.ChronoUnit.HOURS;
+import static java.time.temporal.ChronoUnit.MINUTES;
+import static java.util.Arrays.asList;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -14,10 +17,14 @@ import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.kit.ifv.mobitopp.publictransport.connectionscan.DefaultStopPaths;
 import edu.kit.ifv.mobitopp.publictransport.connectionscan.PublicTransportRoute;
 import edu.kit.ifv.mobitopp.publictransport.connectionscan.RouteSearch;
+import edu.kit.ifv.mobitopp.publictransport.connectionscan.StopPaths;
 import edu.kit.ifv.mobitopp.publictransport.model.Connection;
+import edu.kit.ifv.mobitopp.publictransport.model.RelativeTime;
 import edu.kit.ifv.mobitopp.publictransport.model.Stop;
+import edu.kit.ifv.mobitopp.publictransport.model.StopPath;
 import edu.kit.ifv.mobitopp.publictransport.model.Time;
 
 public class SimpleTransitExampleTest {
@@ -65,6 +72,39 @@ public class SimpleTransitExampleTest {
 		assertThat(route.arrival(), is(equalTo(fourOClock)));
 		assertThat(route.start(), is(equalTo(fromChemnitz)));
 		assertThat(route.end(), is(equalTo(toBerlin)));
+		assertThat(route.connections(), contains(chemnitzToBerlin));
+	}
+	
+	@Test
+	public void routeFromMultipleStartPoints() {
+		Stop chemnitz = network.chemnitz();
+		RelativeTime timeToChemnitz = RelativeTime.of(30, MINUTES);
+		StopPath viaChemnitz = new StopPath(chemnitz, timeToChemnitz);
+		
+		Stop dortmund = network.dortmund();
+		RelativeTime timeToDortmund = RelativeTime.of(1, HOURS);
+		StopPath viaDortmund = new StopPath(dortmund, timeToDortmund);
+
+		Stop berlin = network.berlin();
+		RelativeTime walkTimeInBerlin = RelativeTime.of(5, MINUTES);
+		StopPath toPlaceInBerlin = new StopPath(berlin, walkTimeInBerlin);
+
+		Time atOneOClock = oneOClock;
+		Connection chemnitzToBerlin = network.chemnitzToBerlin();
+		Time includingEgresspath = fourOClock.add(walkTimeInBerlin);
+		
+		StopPaths starts = DefaultStopPaths.from(asList(viaChemnitz, viaDortmund));
+		StopPaths ends = DefaultStopPaths.from(asList(toPlaceInBerlin));
+		
+		RouteSearch connectionScan = network.connectionScan();
+		
+		Optional<PublicTransportRoute> potentialRoute = connectionScan.findRoute(starts, ends, atOneOClock);
+		
+		assertThat(potentialRoute, isPresent());
+		PublicTransportRoute route = potentialRoute.get();
+		assertThat(route.arrival(), is(equalTo(includingEgresspath)));
+		assertThat(route.start(), is(equalTo(chemnitz)));
+		assertThat(route.end(), is(equalTo(berlin)));
 		assertThat(route.connections(), contains(chemnitzToBerlin));
 	}
 }
